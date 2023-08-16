3abbc8a04e48c08bcca74ff6d4cc1bb6b7938420
Friendly-quacking-frog <34441104+Friendly-quacking-frog@users.noreply.github.com>
2023-08-16 16:46:23 +0300

    Made tests for all endpoints, ironed out bugs(hopefully most of them), all tests passed

--- src/main/java/org/shikalin/EmployeeAPITests.java
+++ src/main/java/org/shikalin/EmployeeAPITests.java
@@ -6,7 +6,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.*;
@@ -14,57 +16,41 @@ import org.junit.jupiter.api.*;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmployeeAPITests {
 
-    static HttpClient client = HttpClients.createDefault();
-    static String userToken;
-    static int COMPANY_ID;
-    static int EMPLOYEE_ID;
-
-    static JsonObject postJsonResponseFromRequest(String uri, String params, String userToken) {
-        //POST request to API
-        HttpPost httpPost = new HttpPost(uri);
-        try {
-            if (params != null)
-                httpPost.setEntity(new StringEntity(params));
-            //Send request
-            if (userToken != null)
-                httpPost.addHeader("x-client-token", userToken);
-            HttpResponse response = client.execute(httpPost);
-            //Get response
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                InputStream inStream = entity.getContent();
-                String content = new String(inStream.readAllBytes());
-                JsonObject jsonObject = new Gson().fromJson(content, JsonObject.class);
-                TimeUnit.SECONDS.sleep(1);
-                return jsonObject;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        return null;
-    }
+    Gson gson = new Gson(); //Gson to work with JSONs
+    static HttpClient client; //HttpClient to make requests
+    static String userToken; //User token for POST and PATH requests
+    static int COMPANY_ID; //ID if created company for tests
+    static int EMPLOYEE_ID; //ID of created employee
+    static int TIMEOUT = 4; //Duration of timeout
 
     static JsonObject getJsonResponseFromRequest(String uri, String userToken) {
         //GET request to API
         HttpGet httpGet = new HttpGet(uri);
+        client = HttpClients.createDefault();
         try {
             //Send request
             if (userToken != null)
+                //Add user token if needed
                 httpGet.addHeader("x-client-token", userToken);
             HttpResponse response = client.execute(httpGet);
             //Get response
             int code = response.getStatusLine().getStatusCode();
             HttpEntity entity = response.getEntity();
             if (entity != null) {
+                //Parse response
                 InputStream inStream = entity.getContent();
                 String content = new String(inStream.readAllBytes());
-                TimeUnit.MILLISECONDS.sleep(500);
+                //Make a json object with code and body of request
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("statusCode", code);
                 jsonObject.addProperty("response", content);
+                //Close request
+                entity.getContent().close();
+                //Timeout just in case
+                TimeUnit.SECONDS.sleep(TIMEOUT);
                 return jsonObject;
             }
         } catch (Exception e) {
@@ -74,22 +60,117 @@ public class EmployeeAPITests {
         return null;
     }
 
+    static JsonObject postJsonResponseFromRequest(String uri, String params, String userToken) {
+        //POST request to API
+        HttpPost httpPost = new HttpPost(uri);
+        client = HttpClients.createDefault();
+        try {
+            if (params != null) {
+                // Set JSON body of request
+                httpPost.setEntity(new StringEntity(params, ContentType.APPLICATION_JSON));
+                httpPost.setHeader("content-Type","application/json");
+            }
+            //Send request
+            if (userToken != null)
+                httpPost.addHeader("x-client-token", userToken);
+            //Execute request and get response
+            HttpResponse response = client.execute(httpPost);
+            //Get body of response
+            int code = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                //Parse bytes from response body
+                InputStream inStream = entity.getContent();
+                String content = new String(inStream.readAllBytes());
+                //Convert response to json object
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("statusCode", code);
+                jsonObject.addProperty("response", content);
+                //Close request
+                entity.getContent().close();
+                //Timeout just in case
+                TimeUnit.SECONDS.sleep(TIMEOUT);
+                return jsonObject;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    static JsonObject patchJsonResponseFromRequest(String uri, String params, String userToken) {
+        //POST request to API
+        HttpPatch httpPatch = new HttpPatch(uri);
+        client = HttpClients.createDefault();
+        try {
+            if (params != null) {
+                // Set JSON body of request
+                httpPatch.setEntity(new StringEntity(params, ContentType.APPLICATION_JSON));
+                httpPatch.setHeader("content-Type","application/json");
+            }
+            //Send request
+            if (userToken != null)
+                httpPatch.addHeader("x-client-token", userToken);
+            //Execute request and get response
+            HttpResponse response = client.execute(httpPatch);
+            //Get body of response
+            int code = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                //Parse bytes from response body
+                InputStream inStream = entity.getContent();
+                String content = new String(inStream.readAllBytes());
+                //Convert response to json object
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("statusCode", code);
+                jsonObject.addProperty("response", content);
+                //Close request
+                entity.getContent().close();
+                //Timeout just in case
+                TimeUnit.SECONDS.sleep(TIMEOUT);
+                return jsonObject;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    int getCodeFromResponse(JsonObject jsonObject){
+        //Get status from response
+        return jsonObject.get("statusCode").getAsInt();
+    }
+
+    JsonObject getBodyFromResponse(JsonObject jsonObject){
+        //Get body of response
+        return gson.fromJson(jsonObject.get("response").getAsString(), JsonObject.class);
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        //Before execution of each test, get user token by login
+        //Request parameteres to login
+        String params = "{\"username\": \"leonardo\", \"password\": \"leads\"}";
+        //Send request to get user token
+        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/auth/login", params, null);
+        userToken = gson.fromJson(jsonObject.get("response").getAsString(), JsonObject.class).get("userToken").getAsString();
+    }
 
     @BeforeAll
     static void beforeAll() {
         //Before execution of each test, get user token by login
         //Request parameteres to login
-        String loginParams = "{\"username\": \"leonardo\", \"password\": \"leads\"}";
-        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/auth/login", loginParams, null);
-        if (jsonObject != null) {
-            userToken = jsonObject.get("userToken").getAsString();
-        }
+        Gson gson = new Gson();
+        String params = "{\"username\": \"leonardo\", \"password\": \"leads\"}";
+        //Send request to get user token
+        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/auth/login", params, null);
+        userToken = gson.fromJson(jsonObject.get("response").getAsString(), JsonObject.class).get("userToken").getAsString();
         //Create a test company for employee API tests
-        String companyParams = "{\"name\": \"SHK_Test\", \"description\": \"SHK_Test\"}";
-        jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/company", companyParams, userToken);
-        if (jsonObject != null) {
-            System.out.println(jsonObject);
-        }
+        params = "{\"name\": \"FQF_Test\", \"description\": \"FQF_Test\"}";
+        jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/company", params, userToken);
+        COMPANY_ID = gson.fromJson(jsonObject.get("response").getAsString(), JsonObject.class).get("id").getAsInt();
     }
 
     @AfterAll
@@ -100,17 +181,21 @@ public class EmployeeAPITests {
 
 
     @Test
+    @Order(1)
     @DisplayName("Check response code of retrieving employee list of new company")
     void getEmployeeListCodeTest() {
+        //Send request to get list of employees of company and check response code
         JsonObject jsonObject =
-                getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee?company=" + COMPANY_ID, null);
-        int result = jsonObject.get("statusCode").getAsInt();
+                getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee?company=" + 511, null);
+        int result = getCodeFromResponse(jsonObject);
         assert result == 200;
     }
 
     @Test
+    @Order(2)
     @DisplayName("Check body of response of retrieving employee list of new company")
     void getEmployeeListBodyTest() {
+        //Send request to get list of employees of company and check response body
         JsonObject jsonObject =
                 getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee?company=" + COMPANY_ID, null);
         String result = jsonObject.get("response").getAsString();
@@ -118,19 +203,167 @@ public class EmployeeAPITests {
     }
 
     @Test
+    @Order(3)
     @DisplayName("Check response code of request with incorrect company id")
-    void getWrongEmployeeListCodeTest() {
+    void getEmployeeWrongListCode() {
+        //Send botched request to get list of employees and make sure we get an error
         JsonObject jsonObject =
                 getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee?company=garbage", null);
+        int result = getCodeFromResponse(jsonObject);
+        assert result == 500;
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Response code of request for creating an employee")
+    void testCodeCreateEmployeeTest() {
+        //Create an instance of employee class to convert it into JSON
+        TestEmployee employee = new TestEmployee(COMPANY_ID);
+        //Converting to JSON
+        String params = gson.toJson(employee);
+        //Sending request
+        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/employee", params, userToken);
+        //Get response code and make sure it's correct
+        int code = getCodeFromResponse(jsonObject);
+        assert code == 201;
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Response body of request for creating an employee")
+    void testBodyCreateEmployeeTest() {
+        //Create an instance of employee class to convert it into JSON
+        TestEmployee employee = new TestEmployee(COMPANY_ID);
+        //Converting to JSON
+        String params = gson.toJson(employee);
+        //Sending request
+        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/employee", params, userToken);
+        //Get response code and make sure it's correct
+        EMPLOYEE_ID = getBodyFromResponse(jsonObject).get("id").getAsInt();
+        assert EMPLOYEE_ID > 0;
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Response code of incorrect request for creating an employee")
+    void testCodeCreateWrongEmployeeTest() {
+        //Create an instance of employee class to convert it into JSON
+        TestEmployee employee = new TestEmployee(0);
+        //Converting to JSON
+        String params = gson.toJson(employee);
+        //Sending request
+        JsonObject jsonObject = postJsonResponseFromRequest("https://x-clients-be.onrender.com/employee", params, userToken);
+        //Get response code and make sure it's correct
+        int code = getCodeFromResponse(jsonObject);
+        assert code == 500;
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Get code of respone from requesting employee data by id")
+    void testCodeGetEmployeeById(){
+        //Send request to get list of employees of company and check response code
+        JsonObject jsonObject =
+                getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/" + EMPLOYEE_ID, null);
+        int result = getCodeFromResponse(jsonObject);
+        assert result == 200;
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Check body of response of retrieving employee list of new company")
+    void testBodyGetEmployeeById() {
+        //Send request to get list of employees of company and check response body
+        JsonObject jsonObject =
+                getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/" + EMPLOYEE_ID, null);
+        String result = jsonObject.get("response").getAsString();
+        //Create instance of TestEmployee from request
+        TestEmployee testEmployee = gson.fromJson(result, TestEmployee.class);
+        //compare id and companyId from request with those in tests
+        assert testEmployee.id == EMPLOYEE_ID;
+        assert testEmployee.companyId == COMPANY_ID;
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Get code of respone from requesting employee data by wrong id")
+    void testCodeGetWrongEmployeeById(){
+        //Send request to get list of employees of company and check response code
+        JsonObject jsonObject =
+                getJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/garbage", null);
         int result = jsonObject.get("statusCode").getAsInt();
         assert result == 500;
     }
 
-    //    @Test
-//    @DisplayName("Response code of request for creating an employee")
-    void codeCreateEmployeeTest() {
-        String employeeCreationParams = "{\"id\": 0, \"firstName\": \"Test\", \"lastName\": \"Testovich\", " +
-                "\"middleName\": \"Testov\", \"companyId\": " + COMPANY_ID + ", \"email\": \"string\", \"url\": \"string\"," +
-                " \"phone\": \"string\", \"birthdate\": \"2023-08-15T12:25:25.165Z\", \"isActive\": true}";
+    @Test
+    @Order(10)
+    @DisplayName("Get code of respone from patching employee data by id")
+    void testCodePatchEmployeeById(){
+        //Make JSON with parameters
+        String params = "{\"lastName\": \"Testing\", \"email\": \"Another@email.com\"," +
+                "\"url\": \"phone\", \"phone\": \"url.net\"}";
+        //Send request to get list of employees of company and check response code
+        JsonObject jsonObject =
+                patchJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/" + EMPLOYEE_ID, params, userToken);
+        int result = getCodeFromResponse(jsonObject);
+        assert result == 200;
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("Get body of respone from patching employee data by id")
+    void testBodyPatchEmployeeById(){
+        //Make JSON with parameters
+        String params = "{\"lastName\": \"Testing\", \"email\": \"Another@email.com\"," +
+                "\"url\": \"url.net\", \"phone\": \"string\"}";
+        //Send request to get list of employees of company and check response code
+        JsonObject jsonObject =
+                patchJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/" + EMPLOYEE_ID, params, userToken);
+        TestEmployee result = gson.fromJson(getBodyFromResponse(jsonObject), TestEmployee.class);
+        //Check that correct employee data was changed
+        assert result.id == EMPLOYEE_ID;
+        //Check that fileds were changed
+        //Example response on Swagger drastically differs from actual response,
+        //Half of fields are missing from actual response!
+        assert result.email.equals("Another@email.com");
+        assert result.url.equals("url.net");
+    }
+
+    @Test
+    @Order(12)
+    @DisplayName("Get code from incorrect request to patch emplyee data")
+    void testCodeWrongPatchEmployeeById(){
+        //Make JSON with parameters
+        String params = "{\"lastName\": \"Testing\", \"email\": \"Another@email.com\"," +
+                "\"url\": \"phone\", \"phone\": \"url.net\"}";
+        //Send request to get list of employees of company and check response code
+        JsonObject jsonObject =
+                patchJsonResponseFromRequest("https://x-clients-be.onrender.com/employee/" + 99999999, params, userToken);
+        int result = getCodeFromResponse(jsonObject);
+        assert result == 500;
+    }
+}
+
+class TestEmployee{
+    /***
+     * Class that was made for single purpose:
+     * to be converted to JSON
+     *
+     * It contains some default data
+     */
+
+    int id = 0;
+    String firstName = "Test";
+    String lastName = "Testovich";
+    String middleName = "Testov";
+    int companyId;
+    String email = "FQF@Email.com";
+    String url = "fqf.com";
+    String phone = "string";
+    String birthdate = "2023-08-15T12:25:25.165Z";
+    boolean isActive = true;
+
+    TestEmployee(int companyId){
+        this.companyId = companyId;
     }
 }
